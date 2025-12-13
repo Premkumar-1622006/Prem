d20bffb1c4f155f183a154890735c88c391244c5
Premkumar-1622006 <23bebme047@kahedu.edu.in>
2025-12-13 10:30:49 +0530

    Add files via upload

---
+++ Passorfail.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+   public class Passorfail {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int marks=sc.nextInt();
+        if(marks>=50){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+    
+}
